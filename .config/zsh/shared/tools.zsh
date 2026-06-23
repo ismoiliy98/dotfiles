c5ae05499d6790ebbbd822b5ea1f167bda2c5be1
bek <ismoiliy98@gmail.com>
2026-06-23 10:46:55 -0500

    feat: manage node and bun via brew, drop nvm

--- .config/zsh/shared/tools.zsh
+++ .config/zsh/shared/tools.zsh
@@ -1,21 +1,4 @@
-if [ -d "$HOME/.bun" ]; then
-  export BUN_INSTALL="$HOME/.bun"
-  __add_to_path "$BUN_INSTALL/bin"
-  [ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
-fi
-
-if [ -d "$HOME/.nvm" ]; then
-  export NVM_DIR="$HOME/.nvm"
-  _node_bin=("$NVM_DIR"/versions/node/*/bin(/Nn[-1]))
-  [ -n "$_node_bin" ] && __add_to_path "$_node_bin"
-  unset _node_bin
-  nvm() {
-    unset -f nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-    nvm "$@"
-  }
-fi
+[ -d "$HOME/.bun/bin" ] && __add_to_path "$HOME/.bun/bin"
 
 if [ -d "$HOME/go" ]; then
   export GOPATH="$HOME/go"
